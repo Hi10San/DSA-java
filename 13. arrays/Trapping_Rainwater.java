d55ca8ed878924c7e11994000ed9f4d937bc1b7b
Hi10San <153438379+Hi10San@users.noreply.github.com>
2024-07-18 22:49:58 +0530

    more array

--- 13. arrays/Trapping_Rainwater.java
+++ 13. arrays/Trapping_Rainwater.java
@@ -1,30 +1,35 @@
+import java.util.*;
+
 public class Trapping_Rainwater {
 
     public static int Rainwater(int arr[]){
-        int height = 0;
-        int width = 0;
-        int area = 0;
-        for (int i = 0;i<arr.length;i++){
-            for (int j =0;j<arr.length;j++){
-                if (arr[i] == arr[j]){
-                    height = arr[i];
-                    width +=1;
-                    area = height*width;
-                }
-                if (arr[i]>arr[j]){
-                    height = arr[i]-arr[j];
-                    width = 1;
-                    area = height*width;
-                }
-            }
-            
+        // calculate left max boundary - array
+        int leftMax[] = new int[arr.length];
+        leftMax[0] = arr[0];
+        for(int i=1;i<arr.length;i++){
+            leftMax[i] = Math.max(leftMax[i-1],arr[i]);
         }
-        return area;
+        // calculate rigth max boundary - array
+        int rightMax[] = new int[arr.length];
+        rightMax[arr.length-1] = arr[arr.length-1];
+        for(int i=arr.length-2;i>=0;i--){
+            rightMax[i] = Math.max(rightMax[i+1],arr[i]);
+        }
+        // loop
+        int trappedwater = 0;
+        for(int i=0;i<arr.length;i++){
+        // waterLevel = min(leftmax boundary , rigthmax boundary)
+        int waterLevel = Math.min(leftMax[i],rightMax[i]);
+        // trapped watter = watterLevel - heigth[i]
+        trappedwater += waterLevel - arr[i];
+        }
+        return trappedwater;
 
     }  
+
     public static void main(String[] args) {
         int[] arr={4,2,0,6,3,2,5};
         System.out.println(Rainwater(arr));
     }
 }
-   
+       
